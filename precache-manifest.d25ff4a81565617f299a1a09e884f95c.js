175162172157689f67dcdc2b5d20180a75c18314
Lee Wen Kang <73012553+NPLeeWenKang@users.noreply.github.com>
2020-11-10 21:15:26 +0800

    Add files via upload

---
+++ precache-manifest.d25ff4a81565617f299a1a09e884f95c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b0502659516389ab434357592f06a6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c258a274108e7ff1629f",
+    "url": "/static/css/main.4729915b.chunk.css"
+  },
+  {
+    "revision": "2892ea02c550caaa10b7",
+    "url": "/static/js/2.abfc9123.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.abfc9123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c258a274108e7ff1629f",
+    "url": "/static/js/main.4c239011.chunk.js"
+  },
+  {
+    "revision": "d83f17173cfc0afbc454",
+    "url": "/static/js/runtime-main.fbaf21a7.js"
+  }
+]);
